0bb3616c68793b9797b29516494464ca78df605c
Oleg Shotyk <olegshotyk@gmail.com>
2024-08-28 23:21:46 +0300

    Add project files.

---
+++ SudokuPage/Components/Pages/BoardElement.cs
@@ -0,0 +1,21 @@
+﻿namespace SudokuPage.Components.Pages
+{
+    public class BoardElement
+    {
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        private string _value;
+
+        public string Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+
+    }
+}
---
+++ SudokuPage/Components/Pages/BoardRow.cs
@@ -0,0 +1,22 @@
+﻿namespace SudokuPage.Components.Pages
+{
+    public class BoardRow
+    {
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private List<BoardElement> _rowElements = new List<BoardElement>();
+
+        public List<BoardElement> RowElements
+        {
+            get { return _rowElements; }
+            set { _rowElements = value; }
+        }
+
+    }
+}
